110a7007af336d61775414658f1702388017cdb6
max <bozzhik@ya.ru>
2023-05-27 01:00:11 +0300

    remove react

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import {qrcode} from 'vite-plugin-qrcode'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [qrcode()],
   build: {
     outDir: 'docs',
     assetsDir: 'assets',
